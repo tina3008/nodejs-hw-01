e75619b20c9a3d8d0a1b53fc3f354eeb97f07c23
tina3008 <kicja_3@ukr.net>
2024-07-11 09:14:03 +0300

    format

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -2,9 +2,9 @@ import { PATH_DB } from '../constants/contacts.js';
 import * as fs from 'node:fs/promises';
 
 export const getAllContacts = async () => {
- const rez= await fs.readFile(PATH_DB, {
-    encoding: 'utf8'
- });
+  const rez = await fs.readFile(PATH_DB, {
+    encoding: 'utf8',
+  });
   return JSON.parse(rez);
 };
 
