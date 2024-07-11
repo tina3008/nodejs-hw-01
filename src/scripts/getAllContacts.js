02c99fdb834252f20766ef1cfffc8963cc4ee719
tina3008 <kicja_3@ukr.net>
2024-07-11 08:57:05 +0300

    Update getAllContacts.js

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -2,14 +2,16 @@ import { PATH_DB } from '../constants/contacts.js';
 import * as fs from 'node:fs/promises';
 
 export const getAllContacts = async () => {
-  return await fs.readFile(PATH_DB, {
+ const rez= await fs.readFile(PATH_DB, {
     encoding: 'utf8'
-  });
+ });
+  return JSON.parse(rez);
 };
 
 getAllContacts()
-  .then((data) => JSON.parse(data, undefined, 2))
-  .then((data) => console.log(Array.isArray(data)))
+  // .then((data) => JSON.parse(data, undefined, 2))
+
+  .then((data) => data)
   .catch((error) => console.error(error));
 
 console.log(await getAllContacts());
